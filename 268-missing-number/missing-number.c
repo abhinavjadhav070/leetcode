f55e350b725b121adb4358c9b68c91f10c8de3da
abhinavjadhav070 <itachiuchiha77clan@gmail.com>
2026-07-08 23:33:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (46.23%) - LeetSync

--- 268-missing-number/missing-number.c
+++ 268-missing-number/missing-number.c
@@ -1,8 +1,13 @@
 int missingNumber(int* nums, int numsSize) {
-   int exp=numsSize*(numsSize+1)/2;
-   int act=0;
-   for(int i=0;i<numsSize;i++){
-    act+=nums[i];
-   }
-  return exp-act;
+    // Gauss' Formula for sum of first n numbers: n * (n + 1) / 2
+    int expectedSum = numsSize * (numsSize + 1) / 2;
+    int actualSum = 0;
+    
+    // Calculate the sum of elements present in the array
+    for (int i = 0; i < numsSize; i++) {
+        actualSum += nums[i];
+    }
+    
+    // The difference is the missing number
+    return expectedSum - actualSum;
 }
